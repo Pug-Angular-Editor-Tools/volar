ece0017d0743428cca29d98e1b789aeb50e6e131
mclaughlin_connor <connor1211.mcl@gmail.com>
2022-07-26 16:16:16 +0100

    Remove more unused code

--- extensions/vscode-vue-language-features/src/features/verifyAll.ts
+++
@@ -1,9 +0,0 @@
-import * as vscode from 'vscode';
-import * as shared from '@volar/shared';
-import type { BaseLanguageClient } from 'vscode-languageclient';
-
-export async function register(context: vscode.ExtensionContext, languageClient: BaseLanguageClient) {
-	context.subscriptions.push(vscode.commands.registerCommand('volar.action.verifyAllScripts', () => {
-		languageClient.sendNotification(shared.VerifyAllScriptsNotification.type);
-	}));
-}
